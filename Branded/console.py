002755705d1f5cf39ed1330e73b8ed66d128b56f
𝗡𝗔𝗥𝗨❥♡𝗝𝗔𝗔𝗧 <151159508+narendragitala@users.noreply.github.com>
2024-03-22 07:24:48 +0530

    Update console.py

--- Branded/console.py
+++ Branded/console.py
@@ -46,13 +46,13 @@ COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", ". !").split())
 
 # PM GUARD VARS
 PM_GUARD = bool(getenv("PM_GUARD", True))
-PM_GUARD_TEXT = getenv("PM_GUARD_TEXT", "**🥀 Hey, I am an advanced & superfast high quality userbot assistant with an upgraded version security system.\n\n🌿 I can't let you message my owner's(@mr_naru) dm without my owner's permission.\n\n🌺 My owner is offline now, please wait until my owner allows(thoda sabr kr online aata hu)you.\n\n🍂 Please don't spam here, because spamming will force me to block you from my owner id.**")
+PM_GUARD_TEXT = getenv("PM_GUARD_TEXT", "**🥀 Hey, I am an advanced & superfast high quality userbot assistant with an upgraded version security system.\n\n🌿 I can't let you message my owner's(naru) dm without my owner's permission.\n\n🌺 My owner is offline now, please wait until my owner allows(thoda sabr kr online aata hu)you.\n\n🍂 Please don't spam here, because spamming will force me to block you from my owner id.**")
 PM_GUARD_LIMIT = int(getenv("PM_GUARD_LIMIT", 5))
 
 
 
 # USERBOT DEFAULT IMAGE
-USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://telegra.ph/file/84870d6d89b893e59c5f0.jpg")
+USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://telegra.ph/file/315c20f4a38d1d301b4f7.jpg")
 
 
 
